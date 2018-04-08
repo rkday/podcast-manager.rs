cf2acd26e92017b16e467e03f112e3c7a59458e7
Rob Day <rkd@rkd.me.uk>
2018-04-08 13:38:58 +0100

    Add info window on currently selected podcast

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate rss;
 extern crate htmlescape;
+extern crate textwrap;
 use rss::Channel;
 extern crate tui;
 
@@ -7,24 +8,26 @@ use std::io;
 
 use tui::Terminal;
 use tui::backend::RawBackend;
-use tui::widgets::{Block, Borders, SelectableList, Widget};
+use tui::widgets::{Block, Borders, SelectableList, Widget, Paragraph};
 use tui::layout::{Direction, Group, Rect, Size};
 use tui::style::{Color, Modifier, Style};
+
+
+struct PodcastData {
+    items: Vec<rss::Item>,
+    selected: usize,
+}
 
 fn main() {
     let channel = Channel::from_url("http://www.angryweasel.com/ABTesting/feed/").unwrap();
     let episodes = channel.items().to_vec();
     let episodes_iter = episodes.into_iter();
     let audio_episodes: Vec<rss::Item> = episodes_iter.filter(|i| item_is_audio(i)).collect();
-    //for ep in audio_episodes {
-    //    let desc = ep.description().unwrap_or("(no description)");
-    //    let desc_decoded = htmlescape::decode_html(desc).unwrap();
-        //println!("{}\n{}\n\n", desc_decoded, ep.enclosure().unwrap().url());
-    //}
+    let pd = PodcastData{items: audio_episodes, selected: 4};
 
     let mut terminal = init().expect("Failed initialization");
     let size = terminal.size().unwrap();
-    draw(&mut terminal, size, audio_episodes);
+    draw(&mut terminal, size, &pd);
     let ten_secs = std::time::Duration::from_millis(10000);
     std::thread::sleep(ten_secs);
     terminal.clear().unwrap();
@@ -36,7 +39,12 @@ fn init() -> Result<Terminal<RawBackend>, io::Error> {
 }
 
 
-fn draw(t: &mut Terminal<RawBackend>, size: Rect, audio_episodes: Vec<rss::Item>) {
+fn draw(t: &mut Terminal<RawBackend>, size: Rect, podcasts: &PodcastData) {
+    let ep = &podcasts.items[podcasts.selected];
+    let desc = ep.description().unwrap_or("(no description)");
+    let desc_decoded = htmlescape::decode_html(desc).unwrap();
+    let text = format!("Description:\n{}\n\nURL: {}\n\n", desc_decoded, ep.enclosure().unwrap().url());
+
     Group::default()
         .direction(Direction::Horizontal)
         .sizes(&[Size::Percent(50), Size::Percent(50)])
@@ -44,12 +52,25 @@ fn draw(t: &mut Terminal<RawBackend>, size: Rect, audio_episodes: Vec<rss::Item>
             let style = Style::default().fg(Color::Black).bg(Color::White);
             SelectableList::default()
                 .block(Block::default().borders(Borders::ALL).title("List"))
-                .items(&audio_episodes.iter().map(|ep| format!("XYZ:\r\n    {}", ep.title().unwrap_or("no title"))).collect::<Vec<_>>())
-                .select(3)
+                .items(&podcasts.items.iter().map(|ep| ep.title().unwrap_or("no title")).collect::<Vec<_>>())
+                .select(podcasts.selected)
                 .style(style)
                 .highlight_style(style.clone().fg(Color::LightGreen).modifier(Modifier::Bold))
                 .highlight_symbol(">")
-                .render(t, &chunks[1]);
+                .render(t, &chunks[0]);
+            Group::default()
+                .direction(Direction::Vertical)
+                .sizes(&[Size::Percent(50), Size::Percent(50)])
+                .render(t, &chunks[1], |t, chunks| {
+                    let text2 = format!("{}", textwrap::fill(&text, chunks[0].width as usize));
+                    Paragraph::default()
+                        .block(Block::default().title("Paragraph").borders(Borders::ALL))
+                        .style(Style::default().fg(Color::White).bg(Color::Black))
+                        .wrap(true)
+                        .text(&text2)
+                        .render(t, &chunks[0]);
+                })
+
         });
 
     t.draw().unwrap();
